808a7cb2e248fb93aed014ff8259f882a665051a
Eric <17807730+virtuositeit@users.noreply.github.com>
2018-04-16 13:03:47 -0400

    configure for deployment

--- vrai-home/vr/client.js
+++ vrai-home/vr/client.js
@@ -8,7 +8,7 @@ import {VRInstance} from 'react-vr-web';
 function init(bundle, parent, options) {
   const vr = new VRInstance(bundle, 'WelcomeToVR', parent, {
     // Add custom options here
-    enableHotReload: true,
+    // enableHotReload: true,
   });
   vr.render = function() {
     // Any custom behavior you want to perform on each frame goes here
